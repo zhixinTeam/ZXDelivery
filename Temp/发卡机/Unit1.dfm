90261394dabc5bde472e051bf3a9b744588712bb
ZhaoNing <dmznlin@users.noreply.github.com>
2018-11-28 16:57:30 +0800

    +.添加自助发卡机驱动测试模块.

---
+++ Temp/发卡机/Unit1.dfm
@@ -0,0 +1,79 @@
+object Form1: TForm1
+  Left = 324
+  Top = 340
+  Width = 682
+  Height = 398
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = #23435#20307
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  PixelsPerInch = 96
+  TextHeight = 12
+  object Memo1: TMemo
+    Left = 0
+    Top = 41
+    Width = 666
+    Height = 318
+    Align = alClient
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 0
+  end
+  object Panel1: TPanel
+    Left = 0
+    Top = 0
+    Width = 666
+    Height = 41
+    Align = alTop
+    TabOrder = 1
+    object Button1: TButton
+      Left = 8
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'getCard'
+      TabOrder = 0
+      OnClick = Button1Click
+    end
+    object Button2: TButton
+      Left = 96
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = #25910#21345
+      TabOrder = 1
+      OnClick = Button2Click
+    end
+    object Button3: TButton
+      Left = 184
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = #21457#21345
+      TabOrder = 2
+      OnClick = Button3Click
+    end
+  end
+  object ComPort1: TComPort
+    BaudRate = br9600
+    Port = 'COM1'
+    Parity.Bits = prNone
+    StopBits = sbOneStopBit
+    DataBits = dbEight
+    Events = [evRxChar, evTxEmpty, evRxFlag, evRing, evBreak, evCTS, evDSR, evError, evRLSD, evRx80Full]
+    FlowControl.OutCTSFlow = False
+    FlowControl.OutDSRFlow = False
+    FlowControl.ControlDTR = dtrDisable
+    FlowControl.ControlRTS = rtsDisable
+    FlowControl.XonXoffOut = False
+    FlowControl.XonXoffIn = False
+    Left = 48
+    Top = 88
+  end
+end
---
+++ Temp/发卡机/Unit1.pas
@@ -0,0 +1,93 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls, CPort;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    Panel1: TPanel;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    ComPort1: TComPort;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+  private
+    { Private declarations }
+    procedure Dolog(const nStr: string);
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+uses {UMgrTTCEK720,} USysLoger, UMgrTTCEDispenser, UMgrPoundTunnels;
+
+var
+  gPath: string;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  gPath := ExtractFilePath(Application.ExeName);
+  gSysLoger := TSysLoger.Create(gPath);
+  gSysLoger.LogSync := True;
+  gSysLoger.LogEvent := Dolog;
+
+  gDispenserManager := TDispenserManager.Create;
+  gDispenserManager.LoadConfig(gPath + 'TTCE_K720.xml');
+  gDispenserManager.StartDispensers;
+
+  //gK720ReaderManager := TK720ReaderManager.Create;
+  //gK720ReaderManager.LoadConfig(gPath + 'RFID102.xml');
+  //gK720ReaderManager.StartReader;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  gDispenserManager.StopDispensers;
+end;
+
+procedure TForm1.Dolog(const nStr: string);
+begin
+  Memo1.Lines.Add(nStr);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var nStr: string;
+begin
+  memo1.Lines.Add(gDispenserManager.GetCardNo('gateA', nStr));
+  if nStr <> '' then
+    memo1.Lines.Add(nStr);
+  //xxxxx
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var nStr: string;
+begin
+  gDispenserManager.RecoveryCard('gateA', nStr);
+  if nStr <> '' then
+    memo1.Lines.Add(nStr);
+  //xxxxx
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var nStr: string;
+begin
+  gDispenserManager.SendCardOut('gateA', nStr);
+  if nStr <> '' then
+    memo1.Lines.Add(nStr);
+  //xxxxx
+end;
+
+end.
